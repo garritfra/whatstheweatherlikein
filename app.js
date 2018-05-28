b964ce8d1f7e6137a33dd05efe5a218cfe7fdae1
garritfra <garritfranke@gmail.com>
2018-05-28 23:36:03 +0200

    implement api call

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
+const fetch = require("node-fetch");
 
-console.log("Hello World!");
+if (process.argv.length != 3) {
+  console.log(`Usage: ${process.argv[1]} <Cityname>`);
+  return 1;
+}
+
+const city = process.argv[2];
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}`;
+fetch(url)
+  .then(response => response.json())
+  .then(json => console.log(`The weather in ${city} is ${json.cod}`));
